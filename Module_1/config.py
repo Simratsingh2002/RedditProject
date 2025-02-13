988f25837bb1905da90d5e75bbf30ac326978f46
Simrat Singh Bhatia <ssbhatia@upei.ca>
2025-02-12 21:34:26 -0400

    Initial commit

---
+++ Module_1/config.py
@@ -0,0 +1,19 @@
+# config.py
+
+# This module stores Reddit API credentials.
+
+# Input: None
+# Output: Constants for Reddit API credentials
+# Working: Stores API credentials in variables for use in other modules.
+# Before running main add your API credentials.
+
+REDDIT_CLIENT_ID = ""
+REDDIT_CLIENT_SECRET = ""
+REDDIT_USERNAME = "SimratSingh"
+REDDIT_PASSWORD = ""
+
+
+
+api_key = "" # Add your API key here for sentiment analysis
+
+
